a3a75a53c4c8fd1f0dd2f42b70c994090c1804c0
Emma Kimpe <42942585+EKimpe@users.noreply.github.com>
2018-11-28 09:56:30 +0100

    Updating component.

--- front/src/components/ListeAdresses.jsx
+++ front/src/components/ListeAdresses.jsx
@@ -14,7 +14,7 @@ class ListeAdresses extends Component {
   }
 
   handleFiles = files => {
-    var reader = new FileReader();
+    const reader = new FileReader();
     reader.onload = e => {
       this.setState({
         addressData: reader.result
