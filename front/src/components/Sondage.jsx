2fd07acb9f832909f67c8515bbdad1bbb4a1dd6f
antoinemta <antoine_mta@hotmail.fr>
2018-11-30 13:36:28 +0100

    I restarted and added input.

--- front/src/components/Sondage.jsx
+++ front/src/components/Sondage.jsx
@@ -152,9 +152,9 @@ class Sondage extends Component {
             )}
             <span>Combien de temps (en minutes) mettez-vous pour :</span>
             <br />
-            <label for="questionFore">L’aller (domicile-travail) :</label>
+            <label for="questionFo">L’aller (domicile-travail) :</label>
             <br />
-            <input type="number" id="questionFore" />
+            <input type="number" id="questionFour" />
             <br />
             <label for="questionFive">Le retour (travail-domicile) :</label>
             <br />
@@ -239,7 +239,7 @@ class Sondage extends Component {
               )}
             <label for="questionTen" className="mt-2">
               Vous déplacez-vous à l’extérieur du site à la pause déjeuner
-              (repas, courses…) ?
+              (repas, courses…) :
             </label>
             <br />
             <select id="questionTen">
@@ -248,6 +248,96 @@ class Sondage extends Component {
               <option>Oui, rarement (deux fois par mois)</option>
               <option>Non, jamais</option>
             </select>
+            <br />
+            {1 === true && (
+              <div>
+                <label for="questionEleven" className="mt-2">
+                  En arrivant sur votre lieu de travail, trouvez-vous une
+                  place :
+                </label>
+                <br />
+                <select id="questionEleven">
+                  <option>Très facilement</option>
+                  <option>Facilement</option>
+                  <option>Difficilement</option>
+                  <option>Très difficilement</option>
+                </select>
+              </div>
+            )}
+            {1 === true && (
+              <div>
+                <label for="questionTwelve" className="mt-2">
+                  Les trouvez-vous pratiques, accessibles ? (Oui/Non)
+                </label>
+                <br />
+                <select id="questionTwelve">
+                  <option>Oui</option>
+                  <option>Non</option>
+                </select>
+                <br />
+              </div>
+            )}
+            <label for="questionThirteen">
+              Si votre mode de déplacement principal n’est pas disponible, vers
+              quelle solution vous reportez-vous :
+            </label>
+            <br />
+            <select id="questionThirteen">
+              <option>Voiture personnelle</option>
+              <option>Voiture de fonction/service</option>
+              <option>Covoiturage</option>
+              <option>Train (RER,…)</option>
+              <option>Métro</option>
+              <option>Bus</option>
+              <option>Tramway</option>
+              <option>
+                Deux roues non motorisées dit transport doux (vélo, trottinette,
+                roller...)
+              </option>
+              <option>Deux roues motorisées (moto, scooter)</option>
+              <option>Marche à pied (ou course)</option>
+              <option>Autre</option>
+            </select>
+            <br />
+            <label for="questionFourteen">
+              Combien de déplacements professionnels réalisez-vous en moyenne :
+            </label>
+            <br />
+            <select id="questionFourteen">
+              <option>Je ne fais jamais de déplacements professionnels</option>
+              <option>Quelques fois par an</option>
+              <option>1 à 2 fois par mois</option>
+              <option>1 à 2 fois par semaine</option>
+              <option>Plus de 2 fois par semaine</option>
+            </select>
+            <br />
+            <label for="questionFifteen">
+              Si vous utilisez votre voiture personnelle lors de ces
+              déplacements, pour quelle(s) raison(s) :
+            </label>
+            <br />
+            <select id="questionFifteen">
+              <option>Pas d’autres solutions identifiées</option>
+              <option>Confort</option>
+              <option>Rapidité</option>
+              <option>Flexibilité (si déplacement tôt ou tard)</option>
+              <option>Aucune raison particulière</option>
+            </select>
+            <br />
+            <label for="questionSixteen">
+              Si AG2R LA MONDIALE propose des solutions alternatives en
+              remplacement de votre voiture personnelle, seriez-vous intéressé :
+            </label>
+            <br />
+            <select id="questionSixteen">
+              <option>Oui pour chaque déplacement</option>
+              <option>
+                Oui, si j’ai la possibilité de rentrer chez moi avec
+              </option>
+              <option>Oui, de temps en temps</option>
+              <option>Non, ma voiture m’apporte trop d’avantages</option>
+            </select>
+            <br />
           </form>
         </div>
 
