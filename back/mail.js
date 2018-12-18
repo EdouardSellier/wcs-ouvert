e3d66efa03c3f36248f151c63ad4c0148d932036
Emma Kimpe <emmaemobaka@gmail.com>
2018-12-18 17:01:51 +0100

    Passport beginning.

---
+++ back/mail.js
@@ -0,0 +1,127 @@
+/* const express = require("express");
+const app = express();
+const session = require("express-session");
+const port = 8080;
+const { connection, userTransporter } = require("./conf");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const uuidv4 = require("uuid/v4");
+const nodemailer = require("nodemailer");
+const passport = require("passport");
+
+const PORT_SERVER = 8080;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.post("/inscription", (req, res) => {
+  if (req.body) {
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      req.body.password = hash;
+      const formData = req.body;
+      connection.query("INSERT INTO user SET ?", formData, (err, results) => {
+        if (err) {
+          res.status(500).send("The database crashed ! The reason is " + err);
+        } else {
+          nodemailer.createTestAccount((err, account) => {
+            let transporter = nodemailer.createTransport({
+              host: "smtp.gmail.com",
+              port: 587,
+              secure: false,
+              auth: {
+                user: userTransporter.user,
+                pass: userTransporter.pass
+              }
+            });
+
+            let mailOptions = {
+              from: "no-reply@ouvert.com",
+              to: req.body.mail,
+              subject: "Hello ✔",
+              text: "Hello world?",
+              html: "<b>Hello world?</b>"
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                res
+                  .status(500)
+                  .send(
+                    "An error occured with confirmation e-mail after sign up."
+                  );
+              }
+            });
+          });
+          res.status(201).send("SUCCESS");
+        }
+      });
+    });
+  } else {
+    res.status(403).send("You must provide all informations !");
+  }
+});
+
+app.use(
+  session({
+    genid: req => {
+      return uuidv4();
+    },
+    secret: "this is a random string",
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.post("/connexion", (req, res) => {
+  let mail = req.body.mail;
+  let password = req.body.password;
+  connection.query("SELECT * FROM user WHERE mail = ?", [mail], function(
+    error,
+    results
+  ) {
+    if (error) {
+      res.json({
+        status: false,
+        message: "There are some error with this query"
+      });
+    } else {
+      if (results.length > 0) {
+        bcrypt.compare(password, results[0].password, function(err, ress) {
+          if (!ress) {
+            res.status(500).send("WRONG");
+          } else {
+            let session = uuidv4();
+            res.status(200).send("SUCCESS");
+          }
+        });
+      } else {
+        res.status(404).send("USER DOESN'T EXIST");
+      }
+    }
+  });
+});
+
+app.post("/societyAddress", (req, res) => {
+  console.log(req.body);
+  if (req.body) {
+    res.status(200).send("Success");
+  } else {
+    res.status(500).send("Something went wrong");
+  }
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw new Error("Something bad happened...");
+  }
+  console.log(`Server is listening on ${port}`);
+});
+
+app.listen(PORT_SERVER, () => {
+  console.log(`Listening on http://localhost:${PORT_SERVER}...`);
+}); */
--- back/server.js
+++ back/server.js
@@ -1,128 +1,61 @@
 const express = require("express");
-const app = express();
-const session = require("express-session");
-const port = 8080;
-const { connection, userTransporter } = require("./conf");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
-const uuidv4 = require("uuid/v4");
-const nodemailer = require("nodemailer");
+const passport = require("passport");
+const cors = require("cors");
+const { portServer } = require("./conf");
+require("./passport-strategy");
 
+const app = express();
+
+// ---------------------------------------------------------------- MIDDLEWARES
+
+// enable the reading of 'req.body'
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+
+// CORS management
+app.use(cors());
+
+// -------------------------------------------------------------- PUBLIC ROUTES
+// Login, signup, everything auth-related
+app.use("/auth", require("./auth"));
+
+// Insert here all your public routes. Here is an example
+app.get("/", (req, res) => {
+  // console.log(`Request for 'GET /'`);
+  res.status(200).send("Hello world !");
+});
+
+// -------------------------------------------------------- AUTHENTICATION WALL
+// Each and every request passing here will be stopped (401 Unauthorized) if it doesn't come with proper JWT
+app.all(
+  "/*",
+  passport.authenticate("jwt", { session: false }),
+  (req, res, next) => {
+    next();
+  }
 );
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-app.post("/inscription", (req, res) => {
-  if (req.body) {
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-      req.body.password = hash;
-      const formData = req.body;
-      connection.query("INSERT INTO user SET ?", formData, (err, results) => {
-        if (err) {
-          res.status(500).send("The database crashed ! The reason is " + err);
-        } else {
-          nodemailer.createTestAccount((err, account) => {
-            let transporter = nodemailer.createTransport({
-              host: "smtp.gmail.com",
-              port: 587,
-              secure: false,
-              auth: {
-                user: userTransporter.user,
-                pass: userTransporter.pass
-              }
-            });
+// ------------------------------------------------------------- SECURED ROUTES
+// In these routes, it's useless to check if the user is authenticated, since he passed the AuthWall.
 
-            let mailOptions = {
-              from: "no-reply@ouvert.com",
-              to: req.body.mail,
-              subject: "Hello ✔",
-              text: "Hello world?",
-              html: "<b>Hello world?</b>"
-            };
-
-            transporter.sendMail(mailOptions, (error, info) => {
-              if (error) {
-                res
-                  .status(500)
-                  .send(
-                    "An error occured with confirmation e-mail after sign up."
-                  );
-              }
-            });
-          });
-          res.status(201).send("SUCCESS");
-        }
-      });
-    });
-  } else {
-    res.status(403).send("You must provide all informations !");
-  }
+// Insert here all your protected routes. Here is an example
+app.get("/authrequired", (req, res) => {
+  // console.log(`Request for 'GET /authrequired'`);
+  res.send(`You're in, congrats !\n`);
 });
 
-app.use(
-  session({
-    genid: req => {
-      return uuidv4();
-    },
-    secret: "this is a random string",
-    resave: false,
-    saveUninitialized: true
-  })
-);
-
-app.post("/connexion", (req, res) => {
-  let mail = req.body.mail;
-  let password = req.body.password;
-  connection.query("SELECT * FROM user WHERE mail = ?", [mail], function(
-    error,
-    results,
-    fields
-  ) {
-    if (error) {
-      res.json({
-        status: false,
-        message: "There are some error with this query"
-      });
-    } else {
-      if (results.length > 0) {
-        bcrypt.compare(password, results[0].password, function(err, ress) {
-          if (!ress) {
-            res.status(500).send("WRONG");
-          } else {
-            let session = uuidv4();
-            res.status(200).send("SUCCESS");
-          }
-        });
-      } else {
-        res.status(404).send("USER DON'T EXIST");
-      }
-    }
-  });
+// ------------------------------------------------------------- 404 MANAGEMENT
+app.use((req, res, next) => {
+  res.setHeader("Content-Type", "text/plain");
+  res
+    .status(404)
+    .send(
+      `This page seems too be *really* missing, that's not a way of keeping you out !`
+    );
 });
 
-app.post("/societyAddress", (req, res) => {
-  console.log(req.body);
-  if (req.body) {
-    res.status(200).send("Success");
-  } else {
-    res.status(500).send("Something went wrong");
-  }
-});
-
-app.listen(port, err => {
-  if (err) {
-    throw new Error("Something bad happened...");
-  }
-  console.log(`Server is listening on ${port}`);
+// -------------------------------------------------------------- LAUNCH SERVER
+app.listen(portServer, () => {
+  // console.log(`Listening on http://localhost:${portServer}...`);
 });
