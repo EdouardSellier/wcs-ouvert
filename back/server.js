0ca0dc08c1a9a960473f045cd90e58f77895d0c4
Maureen Vinchent <vinchent.maureen@gmail.com>
2019-01-05 18:54:42 +0100

    Adding new routes and editing mail content for mail sending after survey creation.

--- back/server.js
+++ back/server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const bcrypt = require("bcryptjs");
 const uuidv4 = require("uuid/v4");
 const nodemailer = require("nodemailer");
+const mysql = require("node-mysql");
 
 app.use(bodyParser.json());
 app.use(
@@ -139,7 +140,7 @@ app.post("/rh/send/survey", (req, res) => {
         }
       });
       let mailOptions = {
-        from: "no-reply@ouvert.com",
+        from: '"OUVERT" <no-reply@ouvert.com>',
         to: mail,
         subject: "Sondage de mobilité ✔",
         html: `<h1>Sondage de mobilité</h1><p>Votre employeur vous a envoyé un sondage permettant de mieux connaître vos habitudes de déplacement pour vous rendre sur votre lieu de travail</p><p>Nous vous remercions de bien vouloir y répondre, cela ne prendra que quelques minutes.</p><a href='http://localhost:3000/sondage/${tokenSurvey}'>Cliquez sur ce lien</a><p>Bien à vous,</p><p>L'équipe Mov'R</p>`
@@ -174,22 +175,41 @@ app.post("/admin/payment", (req, res) => {
               pass: userTransporter.pass
             }
           });
-          let mailOptions = {
-            from: "no-reply@ouvert.com",
-            to: req.body.mail,
-            subject: "Hello ✔",
-            text: "Hello world?",
-            html: "<b>Hello world?</b>"
-          };
-          transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-              res
-                .status(500)
-                .send(
-                  "An error occured with confirmation e-mail after sign up."
-                );
-            }
-          });
+          if (formData.has_paid === 1) {
+            let mailOptions = {
+              from: '"OUVERT" <no-reply@ouvert.com>',
+              to: req.body.mail,
+              subject: "Votre inscription est confirmée ✔",
+              html: `<h1>Bienvenue !</h1><p>Nous avons bien reçu votre règlement et vous confirmons que votre inscription a bien été prise en compte. <br/> Vous pouvez dès à présent profiter de nos services en vous connectant à votre compte : <a href='http://localhost:3000/connexion'>Cliquez sur ce lien</a></p><p>Bien à vous,</p><p>L'équipe Mov'R</p>`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                res
+                  .status(500)
+                  .send(
+                    "An error occured with confirmation e-mail after sign up."
+                  );
+              }
+            });
+          } else if (formData.has_paid === 0) {
+            let mailOptions = {
+              from: '"OUVERT" <no-reply@ouvert.com>',
+              to: req.body.mail,
+              subject: "Votre compte n'est plus actif",
+              html: `<h1>Désactivation de votre compte</h1><p>Sauf erreur de notre part, nous n'avons pas reçu votre règlement pour bénéficier de nos services. Nous avons ainsi désactivé votre compte. En cas de problème, vous pouvez nous contacter par e-mail ou par téléphone : <a href='http://localhost:3000/contact'>Cliquez sur ce lien</a>.</p><p>Bien à vous,</p><p>L'équipe Mov'R</p>`
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                res
+                  .status(500)
+                  .send(
+                    "An error occured with confirmation e-mail after sign up."
+                  );
+              }
+            });
+          }
         });
         res.status(200).send("SUCCESS");
       }
@@ -221,6 +241,70 @@ app.get("/admin/list/survey", (req, res) => {
       }
     }
   );
+});
+
+app.post("/admin/list/society", (req, res) => {
+  let totalCount = undefined;
+  connection.query("SELECT COUNT(*) AS TotalCount FROM user", function(
+    err,
+    rows
+  ) {
+    let startNum = 0;
+    let limitNum = 5;
+    if (err) {
+      return err;
+    } else {
+      totalCount = rows[0].TotalCount;
+      startNum = req.body.start;
+      limitNum = req.body.limit;
+    }
+    connection.query(
+      `SELECT company_name, siret, lastname, firstname, mail, company_address, phone_number, has_paid, id FROM user LIMIT ${limitNum} OFFSET ${startNum}`,
+      function(err, result) {
+        if (err) {
+          res.json(err);
+        } else {
+          const allData = {
+            totalCount: totalCount,
+            data: result
+          };
+          res.status(200).json(allData);
+        }
+      }
+    );
+  });
+});
+
+app.post("/admin/list/survey", (req, res) => {
+  let totalCount = undefined;
+  connection.query("SELECT COUNT(*) AS TotalCount FROM survey", function(
+    err,
+    rows
+  ) {
+    let startNum = 0;
+    let limitNum = 5;
+    if (err) {
+      return err;
+    } else {
+      totalCount = rows[0].TotalCount;
+      startNum = req.body.start;
+      limitNum = req.body.limit;
+    }
+    connection.query(
+      `SELECT survey_name, starting_date, ending_date, user_id FROM survey LIMIT ${limitNum} OFFSET ${startNum}`,
+      function(err, result) {
+        if (err) {
+          res.json(err);
+        } else {
+          const allData = {
+            totalCount: totalCount,
+            data: result
+          };
+          res.status(200).json(allData);
+        }
+      }
+    );
+  });
 });
 
 app.listen(port, err => {
