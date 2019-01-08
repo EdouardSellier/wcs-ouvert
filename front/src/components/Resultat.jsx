2d75d23baff068311fdc42e59a51bd39096313c0
antoinemta <antoine_mta@hotmail.fr>
2019-01-08 10:47:32 +0100

    Row and cols added in the component 'ResultBar' of 'Resultat.jsx'

--- front/src/components/Resultat.jsx
+++ front/src/components/Resultat.jsx
@@ -2,7 +2,72 @@ import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import "./css/Resultat.css";
 
-const ResultBar = props => {return();};
+const ResultBar = props => {
+  return (
+    <React.Fragment>
+      <Row className="">
+        <Col md={{ size: 10 }} className="">
+          <Col xs={{ size: 12 }} className="">
+            {props.label}
+          </Col>
+          <Col xs={{ size: 12 }} className="">
+            <Col xs={{ size: 1 }} className="">
+              <Col xs={{ size: 12 }} className="">
+                100%
+              </Col>
+              <Col xs={{ size: 12 }} className="">
+                50%
+              </Col>
+              <Col xs={{ size: 12 }} className="">
+                0%
+              </Col>
+            </Col>
+            <Col xs={{ size: 11 }} className="">
+              {props.possibilities.map(data => (
+                <Col xs={{ size: 1 }} className="">
+                  <Col
+                    xs={{ size: 12 }}
+                    style={{
+                      height:
+                        100 /
+                          (props.dataFetch.length /
+                            props.dataFetch.filter(
+                              dataFetch => dataFetch[props.index] === data
+                            ).length) +
+                        "%",
+                      display: "flex",
+                      alignItems: "flex-end"
+                    }}
+                    className=""
+                  >
+                    <StyledSquare pose={props.hovering ? "end" : "start"}>
+                      {(
+                        100 /
+                        (props.dataFetch.length /
+                          props.dataFetch.filter(
+                            dataFetch => dataFetch[props.index] === data
+                          ).length)
+                      ).toFixed(2) + "%"}
+                    </StyledSquare>
+                  </Col>
+                </Col>
+              ))}
+            </Col>
+          </Col>
+          <Col xs={{ size: 12 }} className="">
+            <Col xs={{ size: 11 }} className="">
+              {props.possibilities.map(data => (
+                <Col xs={{ size: 1 }} className="">
+                  {data}
+                </Col>
+              ))}
+            </Col>
+          </Col>
+        </Col>
+      </Row>
+    </React.Fragment>
+  );
+};
 
 class Resultat extends Component {
   render() {
