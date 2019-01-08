ac1f4f0e40d52cef4fe25be9a12378c79f36eb50
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 16:00:01 +0100

    Adding stuff to passport-strategy.

--- back/passport-strategy.js
+++ back/passport-strategy.js
@@ -5,29 +5,39 @@ const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const { jwtSecret, dbHandle } = require('./conf');
 
-
 passport.use(
-    new LocalStrategy(
-      {
-        usernameField: 'mail',
-        passwordField: 'password'
-      },
-      (mail, password, callback) => {
-        dbHandle.query(
-          `SELECT id, mail, password, lastname, firstname, is_admin, company_name, siret, company_address, phone_number FROM user WHERE mail=${mysql.escape(mail)} LIMIT 1`,
-          (err, results) => {
-              if (err) {
-                  return callback(null, false,{ message: `We crashed, here is the message: ${err}`});
-                }
-                const user = results[0];
-                if (user) {
-                  bcrypt.compare(password, user.password, (errBcrypt, result) => {
-                    if (result === true) {
-                      return callback(
-                        null,
-                        { id: user.id, username: user.mail },
-                        { message: 'Logged In Successfully' }
-                      );
-                    }
-              }
+  new LocalStrategy(
+    {
+      usernameField: 'mail',
+      passwordField: 'password'
+    },
+    (mail, password, callback) => {
+      dbHandle.query(
+        `SELECT id, mail, password, lastname, firstname, is_admin, company_name, siret, company_address, phone_number FROM user WHERE mail=${mysql.escape(
+          mail
+        )} LIMIT 1`,
+        (err, results) => {
+          if (err) {
+            return callback(null, false, { message: `We crashed, here is the message: ${err}` });
           }
+          const user = results[0];
+          if (user) {
+            bcrypt.compare(password, user.password, (errBcrypt, result) => {
+              if (result === true) {
+                return callback(
+                  null,
+                  { id: user.id, username: user.mail },
+                  { message: 'Logged In Successfully' }
+                );
+              }
+              return callback(null, false, {
+                message: 'Incorrect username or password.'
+              });
+            });
+          }
+          return undefined;
+        }
+      );
+    }
+  )
+);
