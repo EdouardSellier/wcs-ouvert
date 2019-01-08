089c0a4dac566ed45301bef14d305ed2982a2ce2
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 15:58:12 +0100

    Adding stuff to passport-strategy.

--- back/passport-strategy.js
+++ back/passport-strategy.js
@@ -12,3 +12,11 @@ passport.use(
         usernameField: 'mail',
         passwordField: 'password'
       },
+      (mail, password, callback) => {
+        dbHandle.query(
+          `SELECT id, mail, password, lastname, firstname, is_admin, company_name, siret, company_address, phone_number FROM user WHERE mail=${mysql.escape(mail)} LIMIT 1`,
+          (err, results) => {
+              if (err) {
+                  return callback(null, false,{ message: `We crashed, here is the message: ${err}`});
+              }
+          }
