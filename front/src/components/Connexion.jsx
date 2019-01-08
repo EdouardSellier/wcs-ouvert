5564e11cb941b5a2f942c0a373beeb39e09c31a1
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 16:50:17 +0100

    Adding connexion.jsx.

---
+++ front/src/components/Connexion.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import "./css/Connexion.css";
+import { Col } from "reactstrap";
+import axios from "axios";
+import NotificationAlert from "react-notification-alert";
