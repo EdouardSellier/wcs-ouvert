0aa1983f8ca63ace41fd3aff431e87fb6298c24e
edelattre <elie1delattre@gmail.com>
2019-01-08 16:52:51 +0100

    Adding dangerMsg in Connexion.jsx in case of wrong imput. Maureen's stuff.

--- front/src/components/Connexion.jsx
+++ front/src/components/Connexion.jsx
@@ -4,3 +4,10 @@ import "./css/Connexion.css";
 import { Col } from "reactstrap";
 import axios from "axios";
 import NotificationAlert from "react-notification-alert";
+
+const dangerMsg = {
+  place: "tr",
+  message: "Votre email et/ou votre mot de passe sont incorrects",
+  type: "danger",
+  autoDismiss: 4
+};
