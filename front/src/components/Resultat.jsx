82621d8e5fd8a7c84901b9d37c55019bb7f84594
antoinemta <antoine_mta@hotmail.fr>
2019-01-08 10:32:06 +0100

    Row added in 'Resultat.jsx'.

--- front/src/components/Resultat.jsx
+++ front/src/components/Resultat.jsx
@@ -4,18 +4,26 @@ import "./css/Resultat.css";
 
 class Resultat extends Component {
   render() {
-    <div className="container-fluid mt-2">
-      <Row>
-        <Col lg={{ size: 2 }}>
-          <button className="mt-2 btn text-white" onClick={this.handleSubmit}>
-            Revenir à l'accueil
-          </button>
-        </Col>
-        <Col lg={{ size: 8 }}>
-          <h3>Consulter les résultats de mon enquête</h3>
-        </Col>
-      </Row>
-    </div>;
+    return (
+      <div className="container-fluid mt-2">
+        <Row>
+          <Col lg={{ size: 2 }}>
+            <button className="mt-2 btn text-white" onClick={this.handleSubmit}>
+              Revenir à l'accueil
+            </button>
+          </Col>
+          <Col lg={{ size: 8 }}>
+            <h3>Consulter les résultats de mon enquête</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={{ size: 12 }}>
+            <b>{this.state.dataFetch.length}</b> salariés ont pour l'instant
+            répondu à ce sondage.
+          </Col>
+        </Row>
+      </div>
+    );
   }
 }
 
