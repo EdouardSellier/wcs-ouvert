414cab98bc7560163b29381bf9d5b2bfb9eb3471
edelattre <elie1delattre@gmail.com>
2019-01-08 16:11:43 +0100

    Generate a signed Json Web Token with the contents of user object and return it in the response.

--- back/auth.js
+++ back/auth.js
@@ -7,10 +7,20 @@ const { jwtSecret, dbHandle, saltRounds } = require('./conf');
 const router = express.Router();
 
 router.post('/connexion', (req, res) => {
-    passport.authenticate('local', { session: false }, (err, user, info) => {
-      if (err || !user) {
-        return res.status(400).json({
-          message: 'Something is not right',
-          user
-        });
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something is not right',
+        user
+      });
+    }
+    req.login(user, { session: false }, loginErr => {
+      if (loginErr) {
+        res.send(loginErr);
       }
+      const token = jwt.sign(user, jwtSecret);
+      return res.status(200).json({ user, token });
+    });
+    return undefined;
+  })(req, res);
+});
