5dc73fcd9def54376c5128aa201aebfb70ee3623
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 15:58:42 +0100

    Adding stuff to passport-strategy.

--- back/passport-strategy.js
+++ back/passport-strategy.js
@@ -18,5 +18,16 @@ passport.use(
           (err, results) => {
               if (err) {
                   return callback(null, false,{ message: `We crashed, here is the message: ${err}`});
+                }
+                const user = results[0];
+                if (user) {
+                  bcrypt.compare(password, user.password, (errBcrypt, result) => {
+                    if (result === true) {
+                      return callback(
+                        null,
+                        { id: user.id, username: user.mail },
+                        { message: 'Logged In Successfully' }
+                      );
+                    }
               }
           }
