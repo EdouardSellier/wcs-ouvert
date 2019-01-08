a86ad0dd50b49de2fe27a3a2ee52ee47784002a4
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 16:09:19 +0100

    Adding router to connexion.

--- back/auth.js
+++ back/auth.js
@@ -5,3 +5,12 @@ const bcrypt = require('bcrypt');
 const { jwtSecret, dbHandle, saltRounds } = require('./conf');
 
 const router = express.Router();
+
+router.post('/connexion', (req, res) => {
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      if (err || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user
+        });
+      }
