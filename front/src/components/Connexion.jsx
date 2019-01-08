1ddd65e336a42fa9da7dcd6f2ef13fb094910fa0
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 17:02:44 +0100

    Finished connexion.jsx.

--- front/src/components/Connexion.jsx
+++ front/src/components/Connexion.jsx
@@ -13,21 +13,21 @@ const dangerMsg = {
 };
 
 class Connexion extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        mail: "",
-        password: ""
-      };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      mail: "",
+      password: ""
+    };
+  }
 
-handleChange = event => {
+  handleChange = event => {
     this.setState({
-        [event.target.name]: event.target.value
+      [event.target.name]: event.target.value
     });
-};  
+  };
 
-alertFunctionDanger = () => {
+  alertFunctionDanger = () => {
     this.refs.notificationAlert.notificationAlert(dangerMsg);
   };
 
@@ -43,3 +43,71 @@ alertFunctionDanger = () => {
       })
       .catch(err => console.log("Error", err));
   };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="connexion mt-3">
+          <NotificationAlert ref="notificationAlert" />
+          <Col
+            lg={{ size: 6, offset: 3 }}
+            md={{ size: 8, offset: 2 }}
+            sm={{ size: 10, offset: 1 }}
+            xs={{ size: 10, offset: 1 }}
+          >
+            <form
+              className="mt-5 formContainer card shadow"
+              onSubmit={this.isLoggedIn}
+            >
+              <h2 className="mt-2 mb-5 text-center">Connexion à mon espace</h2>
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text form-control bg-transparent">
+                      <i className="fa fa-envelope" />
+                    </span>
+                  </div>
+                  <input
+                    type="email"
+                    name="mail"
+                    className="form-control"
+                    id="inputMailConnexion"
+                    ref={ref => (this.inputMailConnexion = ref)}
+                    placeholder="Adresse e-mail"
+                    onChange={this.handleChange}
+                    value={this.state.mail}
+                  />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text form-control bg-transparent">
+                      <i className="fa fa-lock passwordIcon" />
+                    </span>
+                  </div>
+                  <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    id="inputPasswordConnexion"
+                    ref={ref => (this.inputPasswordConnexion = ref)}
+                    placeholder="Mot de passe"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                  />
+                </div>
+              </div>
+              <button type="submit" className="btn text-white mt-3 loginButton">
+                Me connecter
+              </button>
+            </form>
+          </Col>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Connexion;
