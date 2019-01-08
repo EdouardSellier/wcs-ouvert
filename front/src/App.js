93106098358ba433703ea6f8b65c819c6ea11a5b
Emma Kimpe <emmaemobaka@gmail.com>
2019-01-08 17:08:46 +0100

    Adding App.js.

---
+++ front/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Route, BrowserRouter, Switch, Redirect } from "react-router-dom";
+import "./App.css";
+import Accueil from "./components/Accueil";
+import Inscription from "./components/Inscription";
+import Connexion from "./components/Connexion";
+import Contact from "./components/Contact";
+import EspaceRH from "./components/EspaceRH";
+import NouvelleEnquete from "./components/NouvelleEnquete";
+import ListeEnquetesRH from "./components/ListeEnquetesRH";
+import Geolocalisation from "./components/Geolocalisation";
+import Sondage from "./components/Sondage";
+import Resultat from "./components/Resultat";
+import Assistance from "./components/Assistance";
+import EspaceAdmin from "./components/EspaceAdmin";
+import ListeEntreprises from "./components/ListeEntreprises";
+import ListeEnquetes from "./components/ListeEnquetes";
