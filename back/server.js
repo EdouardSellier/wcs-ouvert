12d1bbf47ad83585ca4e2bc82a7732abe9f0378f
edelattre <elie1delattre@gmail.com>
2019-01-08 15:38:20 +0100

    Adding BodyParser & Cors to server.js.

--- back/server.js
+++ back/server.js
@@ -10,34 +10,3 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
-app.use('/auth', require('./auth'));
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hello world !');
-});
-
-// Auth Wall
-app.all('/*', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  next();
-});
-
-app.get('/authrequired', (req, res) => {
-  res.send(`You're in, congrats !\n`);
-});
-
-app.get('/monespace', (req, res) => {
-  console.log(`Request for 'GET /monespace'`);
-  res.status(200).send('Mon espace');
-});
-
-app.use((req, res, next) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res
-    .status(404)
-    .send(`This page seems too be *really* missing, that's not a way of keeping you out !`);
-});
-
-app.listen(portServer, () => {
-  console.log(`Listening on http://localhost:${portServer}...`);
-});
