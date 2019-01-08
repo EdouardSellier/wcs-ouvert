d7b5853a51b195e0f1f44a4b92d3b316f788d222
antoinemta <antoine_mta@hotmail.fr>
2019-01-08 10:19:49 +0100

    File resultat.jsx reseted.

--- front/src/components/Resultat.jsx
+++ front/src/components/Resultat.jsx
@@ -1,40 +1,10 @@
 import React, { Component } from "react";
-import Footer from "./Footer";
 import { Container, Row, Col } from "reactstrap";
 import "./css/Resultat.css";
 
 class Resultat extends Component {
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.history.push("/monespace");
-  };
   render() {
-    return (
-      <div>
-        <div className="resultat">
-          <p className="homeSlogan">
-            MOUV'R : Enquête de mobilité pour vos salariés
-          </p>
-          <hr />
-          <Container className="mt-2">
-            <Row>
-              <Col lg={{ size: 2 }}>
-                <button
-                  className="mt-2 btn text-white"
-                  onClick={this.handleSubmit}
-                >
-                  Revenir à l'accueil
-                </button>
-              </Col>
-              <Col lg={{ size: 8 }}>
-                <h3>Consulter les résultats de mon enquête</h3>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <Footer />
-      </div>
-    );
+    return <div />;
   }
 }
 
