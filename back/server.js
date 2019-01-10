55c31da61ea2ab866c44a486d1b00274cb7dd560
edelattre <elie1delattre@gmail.com>
2019-01-10 09:21:20 +0100

    Adding /user/geolocation route in server.js. Maureen's stuff.

--- back/server.js
+++ back/server.js
@@ -26,6 +26,38 @@ app.get('/monespace', (req, res) => {
   res.status(200).send('Mon espace');
 });
 
+app.post('/user/geolocation', (req, res) => {
+  let positionSociety = req.body.society_position.position.reverse().toString();
+  let addressSociety = req.body.society_position.address.toString();
+  const regex = /\+/g;
+  let correctAddressSociety = addressSociety.replace(regex, ' ');
+  let correctPositionSociety = positionSociety.replace(',', ' - ');
+  let sendSocietyData = `${correctAddressSociety} - (${correctPositionSociety})`;
+  const employeesPositions = req.body.employees_positions;
+  const sendEmployeesData = [];
+  employeesPositions.map(data => {
+    let correctPositionEmployee = data.position
+      .reverse()
+      .toString()
+      .replace(',', ' - ');
+    let addressEmployee = data.address.toString();
+    let correctAddressEmployee = addressEmployee.replace(regex, ' ');
+    let finalData = `${correctAddressEmployee} - (${correctPositionEmployee})`;
+    sendEmployeesData.push(finalData);
+  });
+  const formData = {
+    society_position: sendSocietyData,
+    employees_positions: sendEmployeesData.toString()
+  };
+  connection.query('INSERT INTO geolocation SET ?', formData, (err, results) => {
+    if (err) {
+      res.status(500).send('The database crashed ! The reason is ' + err);
+    } else {
+      res.status(200).send('SUCCESS');
+    }
+  });
+});
+
 app.use((req, res, next) => {
   res.setHeader('Content-Type', 'text/plain');
   res
